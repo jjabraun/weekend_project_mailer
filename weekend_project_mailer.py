7abdd5b0b81b1c3b517a0f33ef6d5a9bacb4e96f
jjabraun <jjabraun@gmail.com>
2017-12-31 16:56:21 -0800

    Initial commit of weekend_project_mailer.py.

---
+++ weekend_project_mailer.py
@@ -0,0 +1,24 @@
+__name__ = 'Weekend Project Mailer'
+__author__ = 'Joshua Braun'
+__copyright__ = 'Copyright 2018, Joshua Braun'
+__credits__ = ['Joshua Braun']
+__license__ = ''
+__version__ = '1.0.0'
+__date__ = '31dec2017'
+__maintainer__ = 'Joshua Braun'
+__email__ = 'jjabraun@gmail.com'
+__status__ = 'Development'
+
+
+class AirtableApi:
+    def __init__(self, **kwargs):
+        pass
+
+
+class MailgunApi:
+    def __init__(self, **kwargs):
+        pass
+
+
+if __name__ == '__main__':
+    pass
